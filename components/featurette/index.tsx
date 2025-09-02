5d472438779e045742cc3425cceb3889135cf32d
LanceJernigan <lance@lancejernigan.com>
2025-09-02 17:59:20 -0400

    Add flower to featuretter

--- components/featurette/index.tsx
+++ components/featurette/index.tsx
@@ -1,6 +1,7 @@
 import { FeaturetteProps } from "./types"
 import styles from "./featurette.module.css";
 import Image from "next/image";
+import Flower from "@/icons/flower";
 
 const Featurette = ({
     title,
@@ -17,7 +18,10 @@ const Featurette = ({
                 <Image src="/assets/images/house.jpeg" alt="The Pink Cactus" width={600} height={400} />
             </div>
             <div className={styles.content}>
-                <h2 className="featurette-title">{title}</h2>
+                <div className={styles.contentHeader}>
+                    <Flower />
+                    <h2 className="featurette-title">{title}</h2>
+                </div>
                 <p className="featurette-description">{description}</p>
             </div>
         </div>
