7686216f4ce9164fc25ae0c4050814205c37d208
LanceJernigan <lance@lancejernigan.com>
2025-09-11 20:35:01 -0400

    Add home link to logo

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,14 +1,20 @@
 import FlowerIcon from "@/icons/flower";
-import styles from "./header.module.css"
+import styles from "./header.module.css";
+import Link from "next/link";
 
 const Header = () => {
-    return (
-        <header className={styles.component}>
-            <div className={styles.wrapper}>
-                <FlowerIcon />
-            </div>
-        </header>
-    );
-}
+	return (
+		<header className={styles.component}>
+			<div className={styles.wrapper}>
+				<Link
+					href="/"
+					className={styles.logo}
+				>
+					<FlowerIcon />
+				</Link>
+			</div>
+		</header>
+	);
+};
 
 export default Header;
