c4b2b8ab447613b8597377e5b82603d4dd2ce3d5
LanceJernigan <lance@lancejernigan.com>
2025-09-19 19:03:53 -0400

    Add header styling on scroll

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,11 +1,36 @@
+"use client";
+
 import FlowerIcon from "@/icons/flower";
 import styles from "./header.module.css";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 // import LogoText from "@/icons/logoText";
 
 const Header = () => {
+	const [hasScrolled, setHasScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY > 0) {
+				setHasScrolled(true);
+			} else {
+				setHasScrolled(false);
+			}
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		handleScroll();
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
 	return (
-		<header className={styles.component}>
+		<header
+			className={styles.component}
+			data-scrolled={hasScrolled}
+		>
 			<div className={styles.wrapper}>
 				<Link
 					href="/"
