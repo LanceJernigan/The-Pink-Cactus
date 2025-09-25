8cd0b582b9222abd70792c9627f1c8e1af25451e
LanceJernigan <lance@lancejernigan.com>
2025-09-25 15:36:12 -0400

    Add site navigation

---
+++ components/navigation/index.tsx
@@ -0,0 +1,84 @@
+import Menu from "@/icons/menu";
+import styles from "./navigation.module.css";
+import { useState } from "react";
+import Link from "next/link";
+import Button from "../button";
+import Instagram from "@/icons/instagram";
+import Facebook from "@/icons/facebook";
+
+const Navigation = () => {
+	const [isOpen, setIsOpen] = useState(true);
+
+	const handleClick = () => {
+		setIsOpen(!isOpen);
+
+		document.body.style.overflow = isOpen ? "auto" : "hidden";
+	};
+
+	return (
+		<>
+			<button
+				className={styles.trigger}
+				onClick={handleClick}
+			>
+				<Menu />
+			</button>
+			<section
+				className={styles.component}
+				data-isopen={isOpen}
+				onClick={handleClick}
+			>
+				<div className={styles.wrapper}>
+					<ul className={styles.links}>
+						<li>
+							<Link href="/">Home</Link>
+						</li>
+						<li>
+							<Link href="/menu">Menu</Link>
+						</li>
+						<li>
+							<Link href="/contact">Contact</Link>
+						</li>
+						<li>
+							<Button
+								onClick={() =>
+									window.open(
+										"https://tables.toasttab.com/restaurants/2c7effac-599a-429d-8dc5-eeb2554d6531/findTime",
+										"_blank",
+									)
+								}
+								raised
+							>
+								Book Room
+							</Button>
+						</li>
+						<li>
+							<ul className={styles.social}>
+								<li>
+									<a
+										href="https://www.instagram.com/thepinkcactusknox/"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<Instagram />
+									</a>
+								</li>
+								<li>
+									<a
+										href="http://www.facebook.com/thepinkcactusknox"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<Facebook />
+									</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default Navigation;
