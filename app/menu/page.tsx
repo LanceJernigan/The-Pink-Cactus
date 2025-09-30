409ae3b42514e705852e9ac203f510f95a7fd605
LanceJernigan <lance@lancejernigan.com>
2025-09-30 17:56:39 -0400

    Pull menu items from Sanity

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,26 +1,44 @@
 import MenuItems from "@/components/menuItems";
-import { cocktails, shooters, arepas, flatbread } from "./data";
+import { client } from "@/sanity/client";
 import styles from "./page.module.css";
 
-export default function Menu() {
+const MENU_ITEMS_QUERY = `*[_type == "menuItems"]{ category, name, price, variations, description }`;
+const MENU_ITEM_CATEGORIES_QUERY = `*[_type == "menuItemCategory"] | order(order asc){
+  _id, title 
+}`;
+
+const options = { next: { revalidate: 30 } };
+
+export default async function Menu() {
+	const menuItems = await client.fetch<
+		{
+			category: { _ref: string };
+			name: string;
+			price: number;
+			variations?: string[];
+			description?: string;
+		}[]
+	>(MENU_ITEMS_QUERY, {}, options);
+	const menuItemCategories = await client.fetch<
+		{ title: string; _id: string }[]
+	>(MENU_ITEM_CATEGORIES_QUERY, {}, options);
+	const mergedMenuItems = menuItemCategories.map((category) => ({
+		...category,
+		items: menuItems.filter((item) => item.category._ref === category._id),
+	}));
+
+	console.log(menuItems, menuItemCategories, mergedMenuItems);
+
 	return (
 		<>
 			<section className={styles.menuItems}>
-				<MenuItems
-					items={cocktails}
-					title="Cocktails"
-				/>
-				<MenuItems
-					items={shooters}
-					title="Shooters"
-				/>
-				<MenuItems
-					items={[
-						{ name: "Arepa", price: 5, variations: arepas },
-						{ name: "Flatbread", price: 12, variations: flatbread },
-					]}
-					title="Food"
-				/>
+				{mergedMenuItems.map(({ title, items }) => (
+					<MenuItems
+						items={items}
+						title={title}
+						key={title}
+					/>
+				))}
 			</section>
 		</>
 	);
