99700f623386535f30d7f4f044c2e829520dd060
LanceJernigan <lance@lancejernigan.com>
2025-10-29 17:30:12 -0400

    FAQs order by rank

--- app/faqs/page.tsx
+++ app/faqs/page.tsx
@@ -2,7 +2,7 @@ import FAQs from "@/components/faqs";
 import { client } from "@/sanity/client";
 import styles from "./page.module.css";
 
-const FAQS_QUERY = `*[_type == "faqs"]{ question, answer }`;
+const FAQS_QUERY = `*[_type == "faqs"]|order(orderRank){ question, answer }`;
 
 const options = { next: { revalidate: 30 } };
 
